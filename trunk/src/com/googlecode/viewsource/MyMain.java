a43a3c3f97a6359ab952e40d164b6ebcbb915959
osyaroslav <osyaroslav@613c00da-85fe-e1b6-84a7-5c9fd220a1c1>
2012-11-17 23:57:01 +0000

    Changed auth method

--- trunk/src/com/googlecode/viewsource/MyMain.java
+++ trunk/src/com/googlecode/viewsource/MyMain.java
@@ -29,6 +29,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.ParseException;
 
 public class MyMain extends Activity
 {
@@ -128,7 +131,7 @@ public class MyMain extends Activity
         }
 
         public String[] getAccessToken(String login, String pass)  {
-            String body="l";
+            String body = "l";
             HttpPost post = null;
             HttpResponse response = null;
             HttpParams params = new BasicHttpParams();
@@ -150,9 +153,9 @@ public class MyMain extends Activity
                 uri = URIUtils.createURI("http", "oauth.vk.com", -1, "/oauth/authorize",
                         URLEncodedUtils.format(qparams, "UTF-8"), null);
             } catch (URISyntaxException e) {
-                return  null;
+                return null;
             }
-            //System.out.println(uri);
+            System.out.println(uri);
             post = new HttpPost(uri);
             try {
                 response = client.execute(post);
@@ -160,21 +163,30 @@ public class MyMain extends Activity
                 return null;  //To change body of catch statement use File | Settings | File Templates.
             }
             post.abort();
-
-            //З відповіді сервера дістаєм заголовок з шляхом
-            String HeaderLocation = response.getFirstHeader("location").getValue();
-
-
-            //З отриманого локейшена дістаємо два параметри "іп_х" і "то_х",
-            //Вони потрібні для авторизації
-            URI RedirectUri = null;
             try {
-                RedirectUri = new URI(HeaderLocation);
-            } catch (URISyntaxException e) {
-                return null;  //To change body of catch statement use File | Settings | File Templates.
+                //З відповіді сервера дістаєм заголовок з шляхом
+                //        
+                body = EntityUtils.toString(response.getEntity());
+            } catch (IOException ex) {
+                Logger.getLogger(MyMain.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(MyMain.class.getName()).log(Level.SEVERE, null, ex);
             }
-            String ip_h = RedirectUri.getQuery().split("&")[2].split("=")[1];
-            String to_h = RedirectUri.getQuery().split("&")[4].split("=")[1];
+            System.out.println(body);
+
+            //Парсим сторінку і дістаєм ip_h i to
+            String form_action = "<input type=\"hidden\" name=\"ip_h\" value=\"";
+            int uri_start_index = body.indexOf(form_action) + form_action.length();
+            int uri_end_index = uri_start_index + body.substring(uri_start_index).indexOf('"');
+            String ip_h = body.substring(uri_start_index, uri_end_index);
+
+            form_action = "<input type=\"hidden\" name=\"to\" value=\"";
+            uri_start_index = body.indexOf(form_action) + form_action.length();
+            uri_end_index = uri_start_index + body.substring(uri_start_index).indexOf('"');
+            String to_h = body.substring(uri_start_index, uri_end_index);
+
+            System.out.println(ip_h);
+            System.out.println(to_h);
 
             //Задаєм параметри для авторизації( деякі параметри константи )
             //ми додаємо тільки іпх, тох, логін, і пасс
@@ -196,6 +208,7 @@ public class MyMain extends Activity
             } catch (URISyntaxException e) {
                 return null;  //To change body of catch statement use File | Settings | File Templates.
             }
+            System.out.println(uri2);
             post = new HttpPost(uri2);
             try {
                 response = client.execute(post);
@@ -203,24 +216,6 @@ public class MyMain extends Activity
                 return null;  //To change body of catch statement use File | Settings | File Templates.
             }
             post.abort();
-
-            //Знову дістаємо локейшн з хедера і знову відправляємо
-            //по суті, це ми руцями проходимо по редіректах
-            //Тут ми відправляєм запит на сторінку дозволу
-            try{
-                HeaderLocation = response.getFirstHeader("location").getValue();
-            }catch (Exception e){
-                return null;
-            }
-
-
-            post = new HttpPost(HeaderLocation);
-            try {
-                response = client.execute(post);
-            } catch (IOException e) {
-                return null;
-            }
-
 
 
 
@@ -232,16 +227,16 @@ public class MyMain extends Activity
             } catch (IOException e) {
                 return null;
             }
-            post.abort();
+
             //Перевіряємо доступ і парсим акшо його нема
             if (body.contains("<form method=\"POST\" action=\"")) {
 
                 //Парсим сторінку і дістаєм силку підтвердження доступу
-                String form_action = "<form method=\"POST\" action=\"";
-                int uri_start_index = body.indexOf(form_action) + form_action.length();
-                int uri_end_index = uri_start_index + body.substring(uri_start_index).indexOf('>') - 1;
+                form_action = "<form method=\"POST\" action=\"";
+                uri_start_index = body.indexOf(form_action) + form_action.length();
+                uri_end_index = uri_start_index + body.substring(uri_start_index).indexOf('>') - 1;
                 String grant_access_uri = body.substring(uri_start_index, uri_end_index);
-
+                System.out.println(grant_access_uri);
                 //Дістали код дозволу, відправляєм
                 post = new HttpPost(grant_access_uri);
                 try {
@@ -250,12 +245,21 @@ public class MyMain extends Activity
                     return null;
                 }
                 post.abort();
+                try {
+                    body = EntityUtils.toString(response.getEntity());
+                } catch (IOException ex) {
+                    Logger.getLogger(MyMain.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ParseException ex) {
+                    Logger.getLogger(MyMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println(body);
 
 
 
             } else {
                 //Якщо ми вже дозволили доступ до додатку то юзаєм це
                 String access_granted = response.getFirstHeader("location").getValue();//.split("?")[1].split("&")[0].split("=")[1];
+                System.out.println("zz" + access_granted);
                 post = new HttpPost(access_granted);
                 try {
                     response = client.execute(post);
@@ -263,13 +267,28 @@ public class MyMain extends Activity
                     return null;
                 }
                 post.abort();
+
+
+                post = new HttpPost(response.getFirstHeader("location").getValue());
+                try {
+                    response = client.execute(post);
+                } catch (IOException e) {
+                    return null;
+                }
+                post.abort();
+
+
             }
 
             //І ось він, ось він код моєї мрії
             //Це буде або Токен або Код, в залежності від першого запиту
-            String[] access_token= new String[2];
+            String[] access_token = new String[2];
             access_token[0] = response.getFirstHeader("location").getValue().split("#")[1].split("&")[0].split("=")[1];
+            System.out.println(access_token[0]);
+
             access_token[1] = response.getFirstHeader("location").getValue().split("user_id=")[1];//.split("&")[0].split("=")[2]; //length-1//
+            System.out.println(access_token[1]);
+
             return access_token;
 
         }
